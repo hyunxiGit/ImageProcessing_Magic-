1a760eba7bc5feb9c14f2d0935b7a0efa028bd8b
hyunxi <hyunxi@hotmail.com>
2018-06-01 12:03:29 -0700

    1. Json 写 string value 2. wstring 转 UTF8 3. json write document to a *.json file

--- ImageProcess/serialize.cpp
+++ ImageProcess/serialize.cpp
@@ -1,17 +1,90 @@
 ﻿#pragma once
 #include "stdafx.h"
+#include "serialize.h"
+#include <fstream>
 
 using namespace rapidjson;
+void Serialize::exportSet(std::map <std::wstring, short> myMap)
+{
+	Document document;
+	Document::AllocatorType &allocator = document.GetAllocator();
+	document.SetObject();
+	for (std::map <std::wstring, short>::iterator it = myMap.begin(); it != myMap.end(); it++)
+	{
+		Value objectId;
+		wchar_t * mywID = (wchar_t *)(it->first).c_str();
+		string myID = Serialize::WStringToUTF8(mywID);
+		
+		const char * myCha = myID.c_str();//string 转 字符串数组
+		char buffer[260];
+		int len = sprintf_s(buffer, "%s", myCha);
+
+		short myIndex = it->second;
+		//document.AddMember(myID, myIndex, allocator);
+		objectId.SetString(buffer, len, document.GetAllocator());
+		document.AddMember(objectId, myIndex, document.GetAllocator());
+	}
+
+	//Serialize::exportJsonFile(document);
+	StringBuffer buffer;
+	Writer<StringBuffer>  writer(buffer);
+	document.Accept(writer);
+	std::string strJson = buffer.GetString();
+
+	std::string strPath = "d:/JsonFile.txt";
+
+	wofstream myLog;
+	myLog.open(strPath, ios::app);
+	if (myLog.is_open())
+	{
+		myLog << buffer.GetString() << endl;
+	}
+}
+ 
 void Serialize::makeJsonObj()
 {
 	Document document;
-	document.SetObject();
-	Document::AllocatorType &allocator = document.GetAllocator(); //获取分配器
-	document.SetObject();
-	document.AddMember("ID", 1, allocator);
+	Document::AllocatorType &allocator = document.GetAllocator();
 
-	printf("%i", document["ID"].GetInt());
+	Value author;
+	string myStr = "lalaji";
+	const char * myCha = myStr.c_str();//string 转 字符串数组
+	char buffer[10];
+	int len = sprintf_s(buffer, "%s", myStr);
+	cout << myCha << endl;
+
+	author.SetString(buffer, len, document.GetAllocator());
+
+	cout << author.GetString() << endl;
+	printf("__________");
+
+	Value val(42);                             // 某 Value
+	document.AddMember(author, val, document.GetAllocator());
+	printf("%i", document[author].GetInt());
+	printf("%i", document["lalaji"].GetInt());
+
 }
+
+void makeJsonObj2()
+{
+	Document document;
+	Document::AllocatorType &allocator = document.GetAllocator();
+
+	Value author;
+	char buffer[10];
+	int len = sprintf_s(buffer, "%s %s", "Milo", "Yip"); // 动态创建的字符串。
+	author.SetString(buffer, len, document.GetAllocator());
+	//memset(buffer, 0, sizeof(buffer));
+	printf("__________");
+
+	Value key("key", document.GetAllocator()); // copy string name
+	Value val(42);                             // 某 Value
+	document.AddMember(author, val, document.GetAllocator());
+	printf("%i", document["Milo Yip"].GetInt());
+
+}
+
+
 void makeJsonObj() 
 {
 	rapidjson :: Document document;
@@ -45,10 +118,10 @@ void makeJsonObj()
 	
 
 }
-void Serialize :: exportJsonFile() 
+void Serialize :: exportJsonFile()
 {
 	cout << "写Json file___________________________________________" << endl;
-	/*
+	Document d;
 	errno_t err;
 	FILE *fp;
 	err = fopen_s(&fp, "d:/我的Json.json", "w");
@@ -60,7 +133,7 @@ void Serialize :: exportJsonFile()
 	{
 	printf("The file 'd:/我的Json.json\n");
 	}
-	rapidjson :: Document d;
+	//rapidjson :: Document d;
 	const char json[] = " { \"hello\" : \"世界噜\", \"t\" : true , \"f\" : false, \"n\": null, \"i\":123, \"pi\": 3.1416, \"a\":[1, 2, 3, 4] } ";
 	d.Parse(json);
 	char writeBuffer[65536];
@@ -68,7 +141,7 @@ void Serialize :: exportJsonFile()
 	rapidjson::Writer<rapidjson::FileWriteStream> writer(os);
 	d.Accept(writer);
 	fclose(fp);
-	*/
+	
 }
 void Serialize ::importJsonFeil()
 {
@@ -94,4 +167,29 @@ void Serialize ::importJsonFeil()
 	assert(d["hello"].IsString());
 	printf("hello = %s\n", d["hello"].GetString());
 	*/
+}
+
+std::string Serialize::WStringToUTF8(const wchar_t* lpwcszWString)
+{
+	char* pElementText;
+	int iTextLen = ::WideCharToMultiByte(CP_UTF8, 0, (LPWSTR)lpwcszWString, -1, NULL, 0, NULL, NULL);
+	pElementText = new char[iTextLen + 1];
+	memset((void*)pElementText, 0, (iTextLen + 1) * sizeof(char));
+	::WideCharToMultiByte(CP_UTF8, 0, (LPWSTR)lpwcszWString, -1, pElementText, iTextLen, NULL, NULL);
+	std::string strReturn(pElementText);
+	delete[] pElementText;
+	return strReturn;
+}
+
+void Serialize::printUTF8(string myUTF8String)
+{
+	system("chcp 65001"); //设置字符集 （使用SetConsoleCP(65001)设置无效，原因未知）  
+
+	CONSOLE_FONT_INFOEX info = { 0 }; // 以下设置字体  
+	info.cbSize = sizeof(info);
+	info.dwFontSize.Y = 16; // leave X as zero  
+	info.FontWeight = FW_NORMAL;
+	wcscpy_s(info.FaceName, L"Consolas");
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), NULL, &info);
+	printf("%s\n", myUTF8String);
 }
--- ImageProcess/serialize.h
+++ ImageProcess/serialize.h
@@ -1,7 +1,15 @@
+#pragma once
+#include "stdafx.h"
+#include <map>
+#include <string>
+
 class Serialize
 {
 public:
 	static void makeJsonObj();
+	static void exportSet(std::map <std::wstring, short>);
 	static void exportJsonFile();
 	static void importJsonFeil();
+	static std::string WStringToUTF8(const wchar_t*);
+	static void printUTF8(std::string);
 };
